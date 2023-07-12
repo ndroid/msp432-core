824bb4b886352d6dcc76d1304ef6d846bc2e2915
chris miller <chris.miller.nd@gmail.com>
2023-07-12 16:24:02 -0400

    moved energia folder to variants directory

---
+++ variants/MSP_EXP432P4111/energia/configPkg/package/cfg/energia_pm4fg.h
@@ -0,0 +1,22 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-D13
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/family/arm/msp432/Timer.h>
+extern const ti_sysbios_family_arm_msp432_Timer_Handle energiaClockTimer;
+
+#include <ti/sysbios/heaps/HeapMem.h>
+extern const ti_sysbios_heaps_HeapMem_Handle heap0;
+
+#include <ti/sysbios/gates/GateMutex.h>
+extern const ti_sysbios_gates_GateMutex_Handle tiposix_mqGate;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
